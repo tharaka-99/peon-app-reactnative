3cebfdcb1816163527bb25280eb9aa7a2f833335
tharaka dilshan <88264479+tharaka-99@users.noreply.github.com>
2022-10-25 13:51:44 +0530

    Update firebase-config.js

--- app/firebase-config.js
+++ app/firebase-config.js
@@ -1,8 +1,9 @@
 import { initializeApp, getApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
-import "firebase/compat/auth";
 import "firebase/compat/firestore";
-
+import "firebase/compat/auth";
+// Initialize Firebase
+//add from the github web
 const firebaseConfig = {
   apiKey: "AIzaSyA-z_kREcNdfv96h9h08kEigpYkXIgwOuQ",
   authDomain: "peon-app-a92ac.firebaseapp.com",
