9fbc8122294f364aa256fd70b83a31a29d322ec7
tharaka dilshan <88264479+tharaka-99@users.noreply.github.com>
2022-10-25 13:25:33 +0530

    Update firebase-config.js

--- app/firebase-config.js
+++ app/firebase-config.js
@@ -3,15 +3,5 @@ import { getFirestore } from "@firebase/firestore";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyA-z_kREcNdfv96h9h08kEigpYkXIgwOuQ",
-  authDomain: "peon-app-a92ac.firebaseapp.com",
-  projectId: "peon-app-a92ac",
-  storageBucket: "peon-app-a92ac.appspot.com",
-  messagingSenderId: "376243520267",
-  appId: "1:376243520267:web:674520eb183ca686d5580e",
-  measurementId: "G-SZ4SXNHMR2",
-};
-
 export const db = getFirestore(app);
 const app = initializeApp(firebaseConfig);
