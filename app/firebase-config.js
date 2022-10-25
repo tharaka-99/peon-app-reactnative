c1e010dfce3a30031fd3ab1095b6b7f50d2c2dc3
tharaka dilshan <88264479+tharaka-99@users.noreply.github.com>
2022-10-25 12:58:22 +0530

    Update firebase-config.js

--- app/firebase-config.js
+++ app/firebase-config.js
@@ -14,7 +14,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
 //add from the github web
+const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
